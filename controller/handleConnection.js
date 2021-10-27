c36774228199311f245c7b0d69ca44406157c8fc
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-27 09:37:27 -0400

    change code length

--- controller/handleConnection.js
+++ controller/handleConnection.js
@@ -2,7 +2,7 @@ import { getCode } from "./../utils/utils.js";
 
 function handleConnection(clientSocket, Rooms, msg) {
   if (Rooms.find((room) => room.quiz === msg.quizId) === undefined) {
-    const gameCode = getCode(3);
+    const gameCode = getCode(6);
     const gameRoom = {
       id: gameCode,
       quiz: msg.quizId,
